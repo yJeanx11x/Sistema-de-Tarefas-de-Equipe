2c2d04e6587026302b6ec3bab33e626f48bf9521
yJeanx11x <j3anlucas010@gmail.com>
2026-03-11 17:33:39 -0300

    fix: hash da senha ao criar usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,15 @@
 const db = require('../module/User')
 const validacao = require('../schemas/userSchema')
+const bcrypt = require('bcrypt')
 
 async function criarUser(req, res, next) {
     const z = validacao.safeParse(req.body)
     try {
+        const passwordHash = await bcrypt.hash(z.data.password, 12)
         await db.create({
             nome: z.data.nome,
             email: z.data.email,
-            password: z.data.password
+            password: passwordHash
         })
         return res.status(201).json({ message: 'Usuario criado com sucesso' })
 
